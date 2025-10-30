d7709ebc3339f4c0aec00eee6f3bb788a6969c48
Aliya praveen <aliyapraveen24@navgurukul.org>
2025-10-30 11:48:33 +0530

    Cors policy changed

--- server.js
+++ server.js
@@ -21,14 +21,13 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 
-// Connect to MongoDB
-connectDB();
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "https://fronend-cpc.onrender.com" || 'http://localhost:5173',
+//   credentials: true
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -80,7 +79,9 @@ app.use('*', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, async() => {
+  // Connect to MongoDB
+  await connectDB();
   console.log(`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
